0cc62f49d9ab9cc55950ab8b3bcaef038699c21f
Arslan-SE <39010565+Arslan-SE@users.noreply.github.com>
2018-09-30 22:22:41 -0400

    Create variables.tf

---
+++ challenges/02-aci-helloworld/variables.tf
@@ -0,0 +1,29 @@
+variable "owner" {
+  description = "User responsible for this cloud environment, resources will be tagged with this"
+}
+
+variable "resourcegroup" {
+  description = "The name of the resource group."
+}
+
+variable "location" {
+  default     = "eastus"
+  description = "The location."
+}
+
+variable "environment" {
+  description = "The environment, i.e. staging, prod, dev, etc."
+}
+variable "cpu" {
+  default     = "0.5"
+  description = "The cpu size of the container."
+}
+
+variable "memory" {
+  default     = "1.5"
+  description = "The memory size of the container."
+}
+
+variable "myusername" {
+  description = "The name of the user"
+}
